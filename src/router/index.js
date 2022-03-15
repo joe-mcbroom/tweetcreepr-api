e8f36128d7f5e7bf28cdf8de569a0cf7a6e2be2d
Joseph McBroom <josephmcbroom@loanerwemcbroom.lan>
2022-03-15 16:13:03 -0400

    update gitignore

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getTweetsByUsername } from '../controller/getTweets.js';
+import { getTweetsByUsername } from '../controller/Tweets.js';
 const router = express.Router();
 
 router.get('/get-tweets/:username', getTweetsByUsername);
