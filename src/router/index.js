4e829d34f56e55c887c207e56a7881eb8bdd3e9f
Joseph McBroom <josephmcbroom@loanerwemcbroom.lan>
2022-03-16 13:00:05 -0400

    cleanup router

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,6 @@ import express from 'express';
 import { getTweetsByUsername } from '../controllers/Tweets.js';
 const router = express.Router();
 
-router.get('/', (req, res) =>
-  res.status(400).json({
-    error: 'Nothing to see here',
-  })
-);
 router.get('/tweets', (req, res) =>
   res.status(400).json({
     error: 'Please specify a username',
